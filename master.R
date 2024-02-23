b164ac18bba957b64982e8c62b270ea1b941606a
MikHoj <tallgrove@gmail.com>
2024-02-23 13:16:06 +0100

    se to sd

--- master.R
+++ master.R
@@ -9,8 +9,21 @@ library(janitor)
 ## import
 data <- read_excel("data/NMA_Data for Mikkel_19.02.2024.xlsx",
                    sheet = "Time points rows",
+                   col_types = c("numeric", "text", "numeric", "text", "text", "text", "text", "text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),
                    na = "--") %>%
         janitor::clean_names()
+
+# SD from SE intervention
+missing_sd <- is.na(data$sd_intervention)
+replacement_values <- data$se_intervention * sqrt(data$n_intervention)
+data$sd_intervention[missing_sd] <- replacement_values[missing_sd]
+data$se_intervention <- NULL
+
+# SD from SE control
+missing_sd <- is.na(data$sd_control)
+replacement_values <- data$se_control * sqrt(data$n_control)
+data$sd_control[missing_sd] <- replacement_values[missing_sd]
+data$se_control <- NULL
 
 
 ## cleaning
